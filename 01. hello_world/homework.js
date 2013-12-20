8bcdef154a2c22ecede47664f1d8e3386e7b8273
fmay <fmay@applicationcraft.com>
2013-12-20 10:41:07 +0000

    test3

--- 01. hello_world/homework.js
+++ 01. hello_world/homework.js
@@ -1,6 +1,6 @@
 // @annotation:tour hello_world
 
-// Write your code below
+// Write your code below   
 // Once you are done (try to avoid looking at it beforehand) you can
 // refer to solution.js to see our suggested solution 
 
